35f75cb18640737a3b9a93b1643d9208ca993346
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2024-03-04 16:33:28 +0200

    feat: added configs, deleted unused svgs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        success: {
+          main: "#00B207",
+        },
+      },
+      padding: {
+        "3.5": "0.875rem",
+      },
+      borderRadius: {
+        "10.5xl": "42px",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
