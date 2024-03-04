df6af79ff0e6c169a71bb5acb7bfb2275e7fb060
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2024-03-04 18:26:43 +0200

    feat: updated routes enum

--- src/shared/enums/routes.enums.ts
+++ src/shared/enums/routes.enums.ts
@@ -1,3 +1,10 @@
 export enum PUBLIC_ROUTES {
   HOME = "/",
+  SHOP = "/shop",
+  PAGES = "/pages",
+  BLOG = "/blog",
+  ABOUT = "/about",
+  CONTACT = "/contact",
+  LOGIN = "/login",
+  REGISTER = "/registration",
 }
