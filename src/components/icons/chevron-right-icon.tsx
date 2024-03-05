10e773077339fa5b994bf898d1f32ee307306f87
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2024-03-05 15:56:13 +0200

    feat: added image with icons

---
+++ src/components/icons/chevron-right-icon.tsx
@@ -0,0 +1,12 @@
+export const ChevronRightIcon = () => {
+  return (
+    <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 0.916341L5.08333 4.99967L1 9.08301"
+        stroke="#999999"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
